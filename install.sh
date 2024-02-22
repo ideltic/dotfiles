61d97a10e32c10e69a9d67fe9f136b47fb04dacc
Natalie Ashley Raine <ideltic@proton.me>
2024-02-22 15:07:11 -0600

    removed plasma compatibility from i3, edited install script

--- install.sh
+++ install.sh
@@ -17,5 +17,7 @@ Beginning installation of needed packages..."
 sleep 5
 
 # installing needed packages
-sudo pacman -S --noconfirm --needed brightnessctl dunst feh firefox flameshot hyfetch i3 kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse lib32-pipewire playerctl polybar rofi sddm starship thunar udiskie xf86-input-libinput xdg-user-dirs xf86-video-nouveau xorg-apps xorg-server xorg-xinput
-yay -S rofi-greenclip rofi-power-menu
+sudo pacman -S --noconfirm --needed bitwarden bottom brightnessctl code discord dunst feh firefox flameshot hyfetch i3 kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse lib32-pipewire neovim p7zip playerctl polybar qt5ct rofi sddm starship thunar udiskie xf86-input-libinput xdg-user-dirs xf86-video-nouveau xorg-apps xorg-server xorg-xinput
+yay -S --noconfirm --needed prismlauncher-qt5-bin rofi-greenclip rofi-power-menu slack-desktop
+
+# [CHANGE THIS] sudo echo "QT5_QPA_PLATFORMTHEME=qt5ct" > /etc/enviroment
