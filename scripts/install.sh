d97a4372857fdf797de919c7331646eeb77d3671
Natalie Ashley Raine <ideltic@proton.me>
2024-02-23 16:33:08 -0600

    added big text to install sit safe to test

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,7 +25,9 @@ echo -e "${PINK}${bold}
      |_____/|______|  |_|   \____/|_|           
 ${normal}${TEXT}
 "
-
+echo -e "${WHITE}${bold}'Natalie Setup' by Natalie :3 (https://github.com/ideltic)"
+echo -e "${TEXT}${normal}Running this script will install, configure, and then apply the Catppuccin theme to all necessary packages."
+echo
 
 echo "This script assumes that you are using a nvidia GPU, and do not have yay installed."
 sleep 4
@@ -34,11 +36,11 @@ sleep 2
 cd
 
 # yay installation
-sudo pacman -Syu --noconfirm --needed base-devel git
-git clone https://aur.archlinux.org/yay.git $HOME/yay && cd $HOME/yay
-makepkg -si
-cd ..
-rm -rf $HOME/yay
+# sudo pacman -Syu --noconfirm --needed base-devel git
+# git clone https://aur.archlinux.org/yay.git $HOME/yay && cd $HOME/yay
+# makepkg -si
+# cd ..
+# rm -rf $HOME/yay
 
 sleep 1
 
@@ -48,9 +50,9 @@ Beginning installation of needed packages..."
 sleep 4
 
 # installing needed packages
-sudo pacman -S --noconfirm --needed bitwarden bottom brightnessctl code discord dunst feh firefox flameshot hyfetch i3 kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse plymouth lib32-pipewire neovim p7zip playerctl polybar qt5ct rofi sddm starship thunar udiskie uwufetch xf86-input-libinput xdg-user-dirs xf86-video-nouveau xorg-apps xorg-server xorg-xinput
+# sudo pacman -S --noconfirm --needed bitwarden bottom brightnessctl code discord dunst feh firefox flameshot hyfetch i3 kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse plymouth lib32-pipewire neovim p7zip playerctl polybar qt5ct rofi sddm starship thunar udiskie uwufetch xf86-input-libinput xdg-user-dirs xf86-video-nouveau xorg-apps xorg-server xorg-xinput
 
-yay -S --noconfirm --needed prismlauncher-qt5-bin rofi-greenclip rofi-power-menu slack-desktop
+# yay -S --noconfirm --needed prismlauncher-qt5-bin rofi-greenclip rofi-power-menu slack-desktop
 
 sleep 1
 
@@ -60,24 +62,24 @@ Creating symbolic links..."
 sleep 4
 
 # creating dotfiles directory, removing original gitclone
-mkdir ~/.dotfiles && mv ~/dotfiles/* ~/.dotfiles && mv ~/dotfiles/.* ~/.dotfiles
-cd ~/.dotfiles && rm -rf ~/dotfiles
+# mkdir ~/.dotfiles && mv ~/dotfiles/* ~/.dotfiles && mv ~/dotfiles/.* ~/.dotfiles
+# cd ~/.dotfiles && rm -rf ~/dotfiles
 
 # symlinking new configurations
-rm -rf ~/.config/dunst && ln -sf ~/.dotfiles/.config/dunst ~/.config/
-rm -rf ~/.config/flameshot && ln -sf ~/.dotfiles/.config/flameshot ~/.config/
-rm -rf ~/.config/hyfetch.json && ln -sf ~/.dotfiles/.config/hyfetch.json ~/.config/
-rm -rf ~/.config/i3 && ln -sf ~/.dotfiles/.config/i3 ~/.config/
-rm -rf ~/.config/kitty && ln -sf ~/.dotfiles/.config/kitty ~/.config/
-rm -rf ~/.config/neofetch && ln -sf /.dotfiles/.config/neofetch ~/.config/
-rm -rf ~/.config/picom && ln -sf ~/.dotfiles/.config/picom ~/.config/
-rm -rf ~/.config/polybar && ln -sf ~/.dotfiles/.config/polybar ~/.config/
-rm -rf ~/.config/rofi && ln -sf ~/.dotfiles/.config/rofi ~/.config/
-rm -rf ~/.config/starship.toml && ln -sf ~/.dotfiles/.config/starship.toml ~/.config/
-rm -rf ~/.config/uwufetch && ln -sf ~/.dotfiles/.config/uwufetch ~/.config/
-rm -f ~/.bashrc && ln -sf ~/.dotfiles/.bashrc ~/.bashrc
-mkdir -p ~/Pictures/backgrounds && ln -sf ~/.dotfiles/backgrounds ~/Pictures/backgrounds
-ln -sf ~/.dotfiles/fonts ~/.local/share/fonts
+# rm -rf ~/.config/dunst && ln -sf ~/.dotfiles/.config/dunst ~/.config/
+# rm -rf ~/.config/flameshot && ln -sf ~/.dotfiles/.config/flameshot ~/.config/
+# rm -rf ~/.config/hyfetch.json && ln -sf ~/.dotfiles/.config/hyfetch.json ~/.config/
+# rm -rf ~/.config/i3 && ln -sf ~/.dotfiles/.config/i3 ~/.config/
+# rm -rf ~/.config/kitty && ln -sf ~/.dotfiles/.config/kitty ~/.config/
+# rm -rf ~/.config/neofetch && ln -sf /.dotfiles/.config/neofetch ~/.config/
+# rm -rf ~/.config/picom && ln -sf ~/.dotfiles/.config/picom ~/.config/
+# rm -rf ~/.config/polybar && ln -sf ~/.dotfiles/.config/polybar ~/.config/
+# rm -rf ~/.config/rofi && ln -sf ~/.dotfiles/.config/rofi ~/.config/
+# rm -rf ~/.config/starship.toml && ln -sf ~/.dotfiles/.config/starship.toml ~/.config/
+# rm -rf ~/.config/uwufetch && ln -sf ~/.dotfiles/.config/uwufetch ~/.config/
+# rm -f ~/.bashrc && ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+# mkdir -p ~/Pictures/backgrounds && ln -sf ~/.dotfiles/backgrounds ~/Pictures/backgrounds
+# ln -sf ~/.dotfiles/fonts ~/.local/share/fonts
 
 sleep 1
 
@@ -87,8 +89,8 @@ Beginning the catppuccin-ification..."
 
 sleep 4
 
-mkdir -v ~/ctp-tempdir && cd ~/ctp-tempdir
+# mkdir -v ~/ctp-tempdir && cd ~/ctp-tempdir
 
-
+# git clones go here
 
 # [CHANGE THIS] sudo echo "QT5_QPA_PLATFORMTHEME=qt5ct" >> ~/.profile
