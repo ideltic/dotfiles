f86e956d9087f64c64e00df4a76b3d6e102098e9
natalie <139081117+ideltic@users.noreply.github.com>
2024-02-25 01:14:56 -0600

    added statement allowing to run commands as user

--- scripts/install.sh
+++ scripts/install.sh
@@ -85,6 +85,25 @@ function catppuccinify {
      sleep 3 && echo
 }
 
+while :
+do
+# stolen from: https://askubuntu.com/a/970898
+# non-root commands must be run as:
+# `sudo -u $real_user <command>`
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
+done
+
+
 clear
 
 echo -e "${PINK}${bold}
