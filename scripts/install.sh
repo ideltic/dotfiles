565effd10384becba6fa22d77fd8c8fbcd464a33
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-05 15:36:30 -0600

    fix(install.sh): edited pkg_install and symlinks to include wayland instead of xorg

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,13 +28,13 @@ function yay_install {
      sleep 3 && echo
 } 
 
-function package_install {
+function pkg_install {
      echo -ne "Installing needed packages.\r" && sleep 1
      echo -ne "Installing needed packages..\r" && sleep 1
      echo -ne "Installing needed packages...\r" && sleep 1 && echo
 
-     #yes all | sudo pacman -S --noconfirm --needed bitwarden bottom brightnessctl code discord dunst feh firefox flameshot hyfetch i3-wm i3lock kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse plymouth neovim p7zip playerctl polybar rofi rofi-emoji sddm starship thunar udiskie uwufetch xdg-user-dirs xf86-input-libinput xf86-video-nouveau xorg-apps xorg-server xorg-xinput
-     #None | yay -S --noconfirm --needed prismlauncher-qt5-bin rofi-greenclip rofi-power-menu slack-desktop ttf-twemoji
+     #yes all | sudo pacman -S --noconfirm --needed bitwarden bottom brightnessctl code dunst firefox grim hyfetch kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse plymouth neovim p7zip playerctl polybar rofi rofi-emoji sddm slurp swayimg swaylock starship thunar udiskie uwufetch vlc waybar wayland xorg-xwayland
+     #None | yay -S --noconfirm --needed discord_arch_electron prismlauncher-qt5-bin rofi-greenclip rofi-power-menu slack-desktop swayfx ttf-twemoji
      
      echo
      echo -e "${GREEN}${bold}Package installation complete.${TEXT}${normal}"
@@ -47,24 +47,24 @@ function symlinks {
      echo -ne "Creating symbolic links...\r" && sleep 1 && echo
 
      # creating dotfiles directory, removing original gitclone
-     #mkdir -v ~/.dotfiles && mv -v ~/dotfiles/* ~/.dotfiles && mv -v ~/dotfiles/.* ~/.dotfiles
-     #cd ~/.dotfiles && rm -rfv ~/dotfiles
+     #mkdir -v ~/.dotfiles && mv -v ~/dotfiles/* ~/.dotfiles && mv -v ~/dotfiles/.* ~/.dotfiles && sleep 0.1
+     #cd ~/.dotfiles && rm -rfv ~/dotfiles && sleep 0.4
 
      # symlinking new configurations
      #rm -rfv ~/.config/dunst && ln -sfv ~/.dotfiles/.config/dunst ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/flameshot && ln -sfv ~/.dotfiles/.config/flameshot ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/hyfetch.json && ln -sfv ~/.dotfiles/.config/hyfetch.json ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/i3 && ln -sfv ~/.dotfiles/.config/i3 ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/kitty && ln -sfv ~/.dotfiles/.config/kitty ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/neofetch && ln -sfv /.dotfiles/.config/neofetch ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/picom && ln -sfv ~/.dotfiles/.config/picom ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/polybar && ln -sfv ~/.dotfiles/.config/polybar ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/rofi && ln -sfv ~/.dotfiles/.config/rofi ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/starship.toml && ln -sfv ~/.dotfiles/.config/starship.toml ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/sway && ln -sfv ~/.dotfiles/.config/sway ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/swayimg && ln -sfv ~/.dotfiles/.config/swayimg ~/.config && sleep 0.1
      #rm -rfv ~/.config/uwufetch && ln -sfv ~/.dotfiles/.config/uwufetch ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/waybar && ln -sfv ~/.dotfiles/.config/waybar ~/.config/ && sleep 0.1
      #rm -fv ~/.bashrc && ln -sfv ~/.dotfiles/.bashrc ~/.bashrc && sleep 0.1
      #mkdir -pv ~/Pictures/backgrounds && ln -sfv ~/.dotfiles/backgrounds ~/Pictures/backgrounds && sleep 0.1
      #ln -sfv ~/.dotfiles/fonts ~/.local/share/fonts
+     #ln -sfv ~/.dotfiles/sh ~/.config/
 
      cd ~
      echo
@@ -85,23 +85,23 @@ function catppuccinify {
      sleep 3 && echo
 }
 
-while :
-do
-# stolen from: https://askubuntu.com/a/970898
-# non-root commands must be run as:
-# `sudo -u $real_user <command>`
-if ! [ $(id -u) = 0 ]; then
-   echo "The script need to be run as root." >&2
-   exit 1
-fi
-
-if [ $SUDO_USER ]; then
-    real_user=$SUDO_USER
-else
-    real_user=$(whoami)
-fi
-
-done
+#while :
+#do
+## stolen from: https://askubuntu.com/a/970898
+## non-root commands must be run as:
+## `sudo -u $real_user <command>`
+#if ! [ $(id -u) = 0 ]; then
+#   echo "The script need to be run as root." >&2
+#   exit 1
+#fi
+#
+#if [ $SUDO_USER ]; then
+#    real_user=$SUDO_USER
+#else
+#    real_user=$(whoami)
+#fi
+#
+#done
 
 
 clear
@@ -130,7 +130,7 @@ sleep 5 && cd
 yay_install
 
 # installing needed packages
-package_install
+pkg_install
 
 # creating symlinks
 symlinks
