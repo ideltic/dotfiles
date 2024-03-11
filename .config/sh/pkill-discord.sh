3303731d1080cbf2bbd659bcedd7fee665709672
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-11 02:43:45 -0500

    feat(sh): added discord-related scripts

---
+++ .config/sh/pkill-discord.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/bash
+
+echo "Closing Discord..."
+
+pkill -f discord
+
+echo "Closed Discord successfully"
---
+++ .config/sh/vencordinstall.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+sh -c "$(curl -sS https://raw.githubusercontent.com/Vendicated/VencordInstaller/main/install.sh)"
+
+
