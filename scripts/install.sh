5f25746f670055e2e4b7acd1979d2399c6b189ad
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-16 01:56:55 -0500

    feat(install.sh): updated package installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -28,13 +28,26 @@ function yay_install {
      sleep 3 && echo
 } 
 
+function wm_install {
+     echo -ne "Installing window manager.\r" && sleep 1
+     echo -ne "Installing window manager..\r" && sleep 1
+     echo -ne "Installing window manager...\r" && sleep 1 && echo
+     
+     #echo "2" | sudo pacman -S --needed --noconfirm autotiling-rs brightnessctl cliphist fuzzel gnome-keyring greetd grim pamixer pipewire pipewire-audio pipewire-pulse plymouth pulseaudio-control qt5-wayland qt6-wayland slurp swayimg swaylock thunar waybar wayland wl-clipboard xorg-xwayland
+     #yay -S --noconfirm --needed bemoji greetd-wlgreet swayfx swaync swayosd-git ttf-twemoji wbg wl-clip-persist-git
+
+     echo
+     echo -e "${GREEN}${bold}Window manager installation complete.${TEXT}${normal}"
+     sleep 3 && echo
+}
+
 function pkg_install {
      echo -ne "Installing needed packages.\r" && sleep 1
      echo -ne "Installing needed packages..\r" && sleep 1
      echo -ne "Installing needed packages...\r" && sleep 1 && echo
 
-     #echo "2" | sudo pacman -S --needed --noconfirm bitwarden bottom brightnessctl code discord dunst firefox greetd grim hyfetch kitty neofetch pamixer picom pipewire pipewire-audio pipewire-pulse plymouth neovim p7zip playerctl polybar rofi rofi-emoji slurp swayimg swaylock starship thunar udiskie uwufetch vlc waybar wayland wl-clipboard xorg-xwayland
-     #yay -S --noconfirm --needed greetd-wlgreet prismlauncher-qt5-bin rofi-greenclip rofi-power-menu swayfx ttf-twemoji wbg
+     #echo "2" | sudo pacman -S --needed --noconfirm bitwarden bottom code discord firefox hyfetch kitty lxappearance neofetch neovim p7zip pacman-contrib playerctl seahorse starship udiskie uwufetch vlc wezterm wezterm-shell-integration wezterm-terminfo
+     #yay -S --noconfirm --needed prismlauncher-qt5-bin
      
      echo
      echo -e "${GREEN}${bold}Package installation complete.${TEXT}${normal}"
@@ -51,18 +64,24 @@ function symlinks {
      #cd ~/.dotfiles && rm -rfv ~/dotfiles && sleep 0.4
 
      # symlinking new configurations
-     #rm -rfv ~/.config/dunst && ln -sfv ~/.dotfiles/.config/dunst/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/fuzzel && ln -sfv ~/.dotfiles/.config/fuzzel/ ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/hyfetch.json && ln -sfv ~/.dotfiles/.config/hyfetch.json ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/kitty && ln -sfv ~/.dotfiles/.config/kitty/ ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/neofetch && ln -sfv ~/.dotfiles/.config/neofetch/ ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/rofi && ln -sfv ~/.dotfiles/.config/rofi/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/nvim && ln -sfv ~/.dotfiles/.config/nvim/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/qt5ct && ln -sfv ~/.dotfiles/.config/qt5ct/ ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/starship.toml && ln -sfv ~/.dotfiles/.config/starship.toml ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/sway && ln -sfv ~/.dotfiles/.config/sway/ ~/.config/ && sleep 0.1
-     #rm -rfv ~/.config/swayimg && ln -sfv ~/.dotfiles/.config/swayimg/ ~/.config && sleep 0.1
+     #rm -rfv ~/.config/swayimg && ln -sfv ~/.dotfiles/.config/swayimg/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/swaync && ln -sfv ~/.dotfiles/.config/swaync/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/swayosd && ln -sfv ~/.dotfiles/.config/swayosd ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/systemd && ln -sfv ~/.dotfiles/.config/systemd ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/uwufetch && ln -sfv ~/.dotfiles/.config/uwufetch ~/.config/ && sleep 0.1
      #rm -rfv ~/.config/waybar && ln -sfv ~/.dotfiles/.config/waybar/ ~/.config/ && sleep 0.1
+     #rm -rfv ~/.config/wlogout && ln -sfv ~/.dotfiles/.config/wlogout ~/.config/ && sleep 0.1
      #rm -fv ~/.bashrc && ln -sfv ~/.dotfiles/.bashrc ~/.bashrc && sleep 0.1
      #rm -fv ~/.wezterm.lua && ln -sfv ~/.dotfiles/.wezterm.lua ~/.wezterm.lua && sleep 0.1
+     #rm -rfv ~/.local/share/fonts && ln -sfv ~/.dotfiles/fonts/ ~/.local/share/ && sleep 0.1
      #sudo rm -rfv /etc/greetd/* && sudo cp -v ~/.dotfiles/greetd/config.toml /etc/greetd/ && sudo cp -v ~/.dotfiles/greetd/sway-config /etc/greetd/ && sleep 0.1
      #sudo cp -v ~/.dotfiles/backgrounds/fuji.png /etc/greetd/ && sudo cp -v ~/.dotfiles/.config/sway/appearance/catppuccin /etc/greetd/ && sleep 0.1
      #mkdir -pv ~/Pictures/backgrounds && ln -sfv ~/.dotfiles/backgrounds ~/Pictures/backgrounds && sleep 0.1
@@ -104,9 +123,14 @@ function catppuccinify {
      git clone https://github.com/catppuccin/tty && cd tty
      # instructions go here
      cd .. && cd .. && sleep 0.2
+
+     # gtk ctp installation
+     git clone https://github.com/catppuccin/gtk && cd gtk
+     # instructions go here
+     cd .. && cd .. && sleep 0.2
+
+     # remove temp directory
      rm -rfv ~/ctptemp
-
-
      
      echo
      echo -e "${GREEN}${bold}Catppuccin-ification complete.${TEXT}${normal}"
@@ -157,7 +181,10 @@ sleep 5 && cd
 # yay installation
 yay_install
 
-# installing needed packages
+# window manager installation
+wm_install
+
+# installing other needed packages
 pkg_install
 
 # creating symlinks
@@ -167,10 +194,8 @@ catppuccinify
 
 # [USE THIS: https://wiki.archlinux.org/title/Uniform_look_for_Qt_and_GTK_applications]
 
-sudo systemctl enable greetd.service && sleep 0.1
-sudo systemctl enable systemd-resolved.service && sleep 0.15
-
-cd
+#sudo systemctl enable greetd.service && sleep 0.1
+#sudo systemctl enable systemd-resolved.service && sleep 0.15
 
 clear && hyfetch
 
