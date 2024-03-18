3e3703e691732d7495d66137f904dc0b386f6d98
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-18 15:54:26 -0500

    feat(swaybar): option to enable battery status

--- .config/sway/swaybar/statuscmd.sh
+++ .config/sway/swaybar/statuscmd.sh
@@ -2,8 +2,9 @@ while true
 do
     time=$(date +'%l:%M%P |')
     VOL="$(pamixer --get-volume)"
-    battery=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}')
-    playing="$(playerctl metadata --format '{{artist}} - {{ title}}')"
+    battery=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}') # battery charge
+    # battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}') # battery status
+    playing="$(playerctl metadata --format '{{artist}}: {{ title}}')"
 
     echo "$playing | VOL: $VOL% | $battery |$time"
 done
