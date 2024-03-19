2203be10a908f14e08a2dfdf221c26bc01718452
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-18 23:53:56 -0500

    feat(swaybar): changed font to compact IosevkaTerm

--- .config/sway/swaybar/statuscmd.sh
+++ .config/sway/swaybar/statuscmd.sh
@@ -4,7 +4,7 @@ do
     VOL="$(pamixer --get-volume)"
     battery=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}') # battery charge
     # battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}') # battery status
-    playing="$(playerctl metadata --format '{{artist}}: {{ title}}')"
+    playing="$(playerctl metadata --format '{{artist}} - {{ title}}')"
 
-    echo "$playing | VOL: $VOL% | $battery |$time"
+    echo "$playing | VOL: $VOL% | $battery | $time"
 done
