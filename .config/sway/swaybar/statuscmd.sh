79dcbd7bfe67e6d2797cbe5e0d30a2a2a42b1a50
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-19 23:25:59 -0500

    fix(swaybar): extra space in front of time

--- .config/sway/swaybar/statuscmd.sh
+++ .config/sway/swaybar/statuscmd.sh
@@ -1,10 +1,10 @@
 while true
 do
-    time=$(date +'%l:%M%P |')
+    time=$(date +'| %l:%M%P |')
     VOL="$(pamixer --get-volume)"
     battery=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}') # battery charge
     # battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}') # battery status
     playing="$(playerctl metadata --format '{{artist}} - {{ title}}')"
 
-    echo "$playing | VOL: $VOL% | $battery | $time"
+    echo "$playing | VOL: $VOL% | $battery $time"
 done
