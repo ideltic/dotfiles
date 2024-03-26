98e67e774a34675bb4ad1ebbaab608834d010772
Natalie Ashley Raine <ideltic05@proton.me>
2024-03-26 14:07:36 -0500

    chore: moved sway & related to archive

---
+++ .config/archive/sway/swaybar/statuscmd.sh
@@ -0,0 +1,10 @@
+while true
+do
+    time=$(date +'| %l:%M%P |')
+    VOL="$(pamixer --get-volume)"
+    battery=$(upower --show-info $(upower --enumerate | grep 'BAT') | grep -E "percentage" | awk '{print $2}') # battery charge
+    # battery_status=$(upower --show-info $(upower --enumerate | grep 'BAT') | egrep "state" | awk '{print $2}') # battery status
+    #playing="$(playerctl metadata --format '{{artist}} - {{ title}}')"
+
+    echo "$playing | VOL: $VOL% | $battery $time"
+done
