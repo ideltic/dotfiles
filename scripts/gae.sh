360b01999c381d27f57ec47df0d357ca22abb780
Natalie Raine <nyatalieeee@proton.me>
2024-07-15 11:55:48 -0500

    still borked

--- scripts/gae.sh
+++ scripts/gae.sh
@@ -17,17 +17,20 @@ clear
 if [ ! -f /usr/bin/figlet ]
 then
     echo -e "${YELLOW}The program \"figlet\" is necessary for the script, but it was not found.${TEXT}\n"
-    read -p "Would you like to install it now? (y/n): " -n 1 -r $INSTALL && echo
+    read -p "Would you like to install it now? (y/n): " -n 1 -r $INSTALL
+
+    case $INSTALL in
+        y)
+            clear
+            echo -e "Installing figlet...\n" && sleep 1.5
+            sudo pacman -S --needed --noconfirm figlet
+            echo -e "${GREEN}\nFiglet installed!" && sleep 2
+            ;;
+        n)
+            echo -e "\nExiting..." && sleep 0.05
+            exit 1
+            ;;
+    esac
 fi
-case $INSTALL in
-    y|Y )
-        clear
-        echo -e "\nInstalling figlet..." && sleep 2
-        sudo pacman -S --needed figlet
-        echo -e "${GREEN}\nFiglet installed!" && sleep 2
-        ;;
-    n|N )
-        echo -e "\nExiting..." && sleep 0.05
-        exit 1
-        ;;
-esac
+
+echo -e "\nstill borked"
