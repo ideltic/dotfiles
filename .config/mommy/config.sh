8df5c59d9e936d51f88e85c009f08698fec13fa9
Natalie Raine <nyatalieeee@proton.me>
2024-07-30 11:09:59 -0500

    add mommy
    no regrets

---
+++ .config/mommy/config.sh
@@ -0,0 +1,7 @@
+MOMMY_CAPITALIZE=0
+MOMMY_ENCOURAGEMENTS=0
+MOMMY_FORBIDDEN_WORDS="big fat kiss"
+MOMMY_PRONOUNS="she her her"
+MOMMY_SUFFIX="~/ :3/ <3"
+
+MOMMY_COMPLIMENTS_EXTRA="you're being such a good girl" # TODO: add more
