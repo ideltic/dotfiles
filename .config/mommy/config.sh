925c9cc366c036cb2c584a77b095eb85a6576a5f
Natalie Raine <nyatalieeee@proton.me>
2024-07-31 11:51:38 -0500

    . . .

--- .config/mommy/config.sh
+++ .config/mommy/config.sh
@@ -4,4 +4,4 @@ MOMMY_FORBIDDEN_WORDS="big fat kiss"
 MOMMY_PRONOUNS="she her her"
 MOMMY_SUFFIX="~/ :3/ <3"
 
-MOMMY_COMPLIMENTS_EXTRA="you're being such a good girl" # TODO: add more
+MOMMY_COMPLIMENTS_EXTRA="you're being such a good girl/you're making mommy so happy/mommy loves you/well done~! mommy is so happy for you" # TODO: add more
